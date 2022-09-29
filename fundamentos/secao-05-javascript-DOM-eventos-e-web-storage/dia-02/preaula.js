0d951582a9e55e27ada840ab815f92aa0014d82d
Kamosara <rafael.s.m.cardoso@hotmail.com>
2022-09-29 16:06:12 -0300

    exercicios pre aula

---
+++ fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-02/preaula.js
@@ -0,0 +1,15 @@
+ondeestapai = document.getElementById('elementoOndeVoceEsta').parentNode;
+ondeestapai.color = 'blue';
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Adiciona Texto';
+document.getElementById('pai').firstChild;
+document.getElementById('elementoOndeVoceEsta').parentNode;
+
+let criairmao = document.createElement('section');
+document.querySelector('#pai').appendChild(criairmao);
+
+let rec = document.querySelector('#elementoOndeVoceEsta');
+
+let cria = document.createElement('section');
+
+rec.appendChild(cria);
+
